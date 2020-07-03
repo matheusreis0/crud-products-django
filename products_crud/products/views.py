03656a5238b6aa58a1b09ca6ee9cd2f1ff54dd92
Matheus Reis <matheus.reis@olist.com>
2020-07-03 11:03:28 -0300

    Added product view class

--- products_crud/products/views.py
+++ products_crud/products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+class ProductAPI(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('id')
+    serializer_class = ProductSerializer
