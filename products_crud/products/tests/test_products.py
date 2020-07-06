e620d631937dd936c47ee571fb8d4fb784ec71b5
Matheus Reis <matheus.reis@olist.com>
2020-07-06 15:58:13 -0300

    Added more tests for status_code verification

--- products_crud/products/tests/test_products.py
+++ products_crud/products/tests/test_products.py
@@ -50,21 +50,101 @@ class ProductTest(TestCase):
         response = self.client.post(self.url, data=product_create_json, 
             content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_return_id_after_post_product(self):
+        """
+            Test to check if response returns the id of the new product
+        """
+        product_create_json = {
+            "name": "Product Test",
+            "price": 123.0
+        }
+        response = self.client.post(self.url, data=product_create_json, 
+            content_type='application/json')
+        content = response.data
+        product = Product.objects.latest('id')
+
+        self.assertEqual(content["id"], product.id)
+
+    def test_missing_name_post_product(self):
+        """
+            Test to check if returns an error for POST of product without name
+        """
+        product_create_json = {
+            "price": 123.0
+        }
+        response = self.client.post(self.url, data=product_create_json, 
+            content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_missing_price_post_product(self):
+        """
+            Test to check if returns an error for POST of product without price
+        """
+        product_create_json = {
+            "name": "Product"
+        }
+        response = self.client.post(self.url, data=product_create_json, 
+            content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_missing_data_post_product(self):
+        """
+            Test to check if returns an error for POST of product without data
+        """
+        product_create_json = {}
+        response = self.client.post(self.url, data=product_create_json, 
+            content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_update_one_product(self):
         """
             Test PUT method to one product
         """
         product_id = '1'
-        product_create_json = {
+        product_update_json = {
             "name": "Product Test",
             "price": 123.0
         }
         response = self.client.put(f"{self.url}{product_id}/", 
-            data=product_create_json, content_type='application/json')
+            data=product_update_json, content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_missing_data_put_product(self):
+        """
+            Test to check if returns an error for PUT request without data
+        """
+        product_id = '1'
+        product_update_json = {}
+        response = self.client.put(f"{self.url}{product_id}/", 
+            data=product_update_json, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_patch_one_product(self):
+    def test_missing_name_put_product(self):
+        """
+            Test to check if returns an error for PUT method without name
+        """
+        product_id = '1'
+        product_update_json = {
+            "price": 123.0
+        }
+        response = self.client.put(f"{self.url}{product_id}/", 
+            data=product_update_json, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_missing_price_post_product(self):
+        """
+            Test to check if returns an error for PUT method without price
+        """
+        product_id = '1'
+        product_update_json = {
+            "name": "Product"
+        }
+        response = self.client.put(f"{self.url}{product_id}/", 
+            data=product_update_json, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_update_patch_method_one_product(self):
         """
             Test PATCH method to one product
         """
